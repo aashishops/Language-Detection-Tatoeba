74155b57b47ee97478362e7da674efd9c34714ef
aashishops <107553285+aashishops@users.noreply.github.com>
2023-09-21 02:26:30 +0530

    Updated streamlit app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ st.title("LANGUAGE DETECTION :scroll:")
 st.subheader("This is a Language Detector.")
 st.write("Type a sentence in the below box to identify the Language.")
 text = st.text_area("Enter the sentence to identify")
-with open('./lang_model','rb')as f:
+with open('lang_model','rb')as f:
     mod= pickle.load(f)
 prediction = mod.predict([text])[0]
 prediction = ''.join(prediction)
-df = pd.read_csv(r'./lang_abr.csv')
+df = pd.read_csv(r'lang_abr.csv')
 abr = df[(df["lang"]==prediction)]
 
 if st.button("Detect"):
