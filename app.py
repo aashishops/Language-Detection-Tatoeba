c61d87dc1ead352f4c45d3fa75b1fbb88ec72e54
aashishops <107553285+aashishops@users.noreply.github.com>
2023-09-21 04:08:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ st.title("LANGUAGE DETECTION :scroll:")
 st.subheader("This is a Language Detector.")
 st.write("Type a sentence in the below box to identify the Language.")
 text = st.text_area("Enter the sentence to identify")
-with open('lang_model.pkl','rb')as f:
-    mod= pickle.load(f)
+with open('lang_model.pkl','rb')as file:
+    mod= pickle.load(file)
 prediction = mod.predict([text])[0]
 prediction = ''.join(prediction)
 df = pd.read_csv(r'lang_abr.csv')
