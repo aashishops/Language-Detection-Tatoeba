6b521bbf8d596aba5a500faab32a3163a66adbde
aashishops <107553285+aashishops@users.noreply.github.com>
2023-09-21 11:59:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import pandas as pd
-import joblib
 st.set_page_config(page_title='Language Detection')
-
 st.title("LANGUAGE DETECTION :scroll:")
 st.subheader("This is a Language Detector.")
 st.write("Type a sentence in the below box to identify the Language.")
 text = st.text_area("Enter the sentence to identify")
-mod = joblib.dump('language_model.pkl')
+with open('language_model.pkl','rb')as f:
+    mod= pickle.load(f)
 prediction = mod.predict([text])[0]
 prediction = ''.join(prediction)
 df = pd.read_csv(r'lang_abr.csv')
 abr = df[(df["lang"]==prediction)]
-
 if st.button("Detect"):
     st.write('The Detected Language is ',abr['abr'].item(),'.')
     
