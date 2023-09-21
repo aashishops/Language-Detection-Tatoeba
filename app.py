c435730252a2ad8ce613dcb01458e0f3a45a8ee4
aashishops <107553285+aashishops@users.noreply.github.com>
2023-09-21 11:23:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import pickle
 import pandas as pd
-
+from sklearn.externals import joblib
 st.set_page_config(page_title='Language Detection')
 
 st.title("LANGUAGE DETECTION :scroll:")
 st.subheader("This is a Language Detector.")
 st.write("Type a sentence in the below box to identify the Language.")
 text = st.text_area("Enter the sentence to identify")
-with open('language_model.pkl','rb')as file:
-    mod= pickle.load(file)
+mod = joblib.dump('language_model.pkl')
 prediction = mod.predict([text])[0]
 prediction = ''.join(prediction)
 df = pd.read_csv(r'lang_abr.csv')
